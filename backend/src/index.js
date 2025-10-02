5d426031d6ddd1b9c8a5f6e117e2e750ea574b0e
Anas <anaskhan2209k@gmail.com>
2025-10-03 01:17:17 +0530

    completed backend

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import authRouter from './routes/auth.route.js'
+import messageRouter from './routes/message.route.js'
+import { connectDB } from "./utils/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+app.use('/api/auth', authRouter);
+app.use('/api/message', messageRouter);
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+    connectDB()
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='mt-10 mr-4 mb-8 ml-8 text-blue-500'>Hello World</div>
+  )
+}
+
+export default App
